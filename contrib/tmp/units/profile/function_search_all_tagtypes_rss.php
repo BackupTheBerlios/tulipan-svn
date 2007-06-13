c66a9b9601987953bd3617eda5ffa5c2474b5b93
xtingray <xtingray@6885239f-cc32-0410-aacc-fe9b53ed630a>
2007-06-13 23:47:31 +0000

    Cleaning code

--- contrib/tmp/units/profile/function_search_all_tagtypes_rss.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    foreach($data['profile:details'] as $profiletype) {
-        if ($profiletype->field_type == "keywords") {
-            $data['search:tagtypes:rss'][] = $profiletype->internal_name;
-        }
-    }
-
-?>
--- contrib/tmp/units/users/get_type.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// Returns the user_type of a particular user as specified in $parameter
-
-    $run_result = user_type($parameter);
-        
-?>
