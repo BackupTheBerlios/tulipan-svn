751a8705fc037c7dea819b6af6b009f15e17672f
johanqnms <johanqnms@6885239f-cc32-0410-aacc-fe9b53ed630a>
2007-10-22 21:28:32 +0000

    Subiendo nueva funcionalidad

---
+++ contrib/tmp/javascript/mootools/data_hr.js
@@ -0,0 +1,12 @@
+{"previews":[
+	{"name":"Blue Earth", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/earth-blue.jpg", "description":"A blue version of Earth.", "views":200},
+	{"name":"User Profile", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/user.jpg", "description":"A chubby user guy.", "views":0},
+	{"name":"Mouse", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/mouse.jpg", "description":"Standard mouse icon.", "views":99},
+	{"name":"Gold Earth", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/earth-gold.jpg", "description":"A gold version of Earth.", "views":7855},
+	{"name":"Page and Pencil", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/edit.jpg", "description":"A piece of paper and a pencil.", "views":5},
+	{"name":"Speaker", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/speaker.jpg", "description":"A speaker.", "views":16},
+	{"name":"Orange Monitor", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/monitor.jpg", "description":"A yellow, glowing monitor.", "views":355},
+	{"name":"Software Box", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/package.jpg", "description":"A nice wrapped package in a box.", "views":23452},
+	{"name":"FIRE!!!!", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/fire.jpg", "description":"Orange flame.", "views":395},
+	{"name":"Screwdriver Paper", "src":"http://localhost/tmp/mod/template/templates/Default_Template/images_js/setting.jpg", "description":"A screwdriver in front of a piece of paper.", "views":42}
+]}
